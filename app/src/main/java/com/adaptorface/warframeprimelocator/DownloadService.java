8a7590f44f5313a9d71dd12a38afd46417ba191e
Kristoffer Rogne <Kristoffer_rogne@hotmail.com>
2016-09-11 12:16:17 +0200

    test

---
+++ app/src/main/java/com/adaptorface/warframeprimelocator/DownloadService.java
@@ -0,0 +1,71 @@
+package com.adaptorface.warframeprimelocator;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.ResultReceiver;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class DownloadService extends IntentService {
+    public static final int UPDATE_PROGRESS = 8344;
+    public DownloadService() {
+        super("DownloadService");
+    }
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String urlToDownload = intent.getStringExtra("url");
+        ResultReceiver receiver = intent.getParcelableExtra("receiver");
+        try {
+            URL url = new URL(urlToDownload);
+            URLConnection connection = url.openConnection();
+            connection.connect();
+            // this will be useful so that you can show a typical 0-100% progress bar
+            int fileLength = connection.getContentLength();
+
+            // download the file
+            File file = new File (getFilesDir().getAbsolutePath(), "MissionDecks.txt");
+            file.createNewFile();
+            InputStream input = new BufferedInputStream(connection.getInputStream());
+            OutputStream output = new FileOutputStream(file);
+
+            byte data[] = new byte[1024];
+            long total = 0;
+            int count;
+            while ((count = input.read(data)) != -1) {
+                total += count;
+                // publishing the progress....
+                Bundle resultData = new Bundle();
+                resultData.putInt("progress" ,(int) (total * 100 / fileLength));
+                receiver.send(UPDATE_PROGRESS, resultData);
+                output.write(data, 0, count);
+            }
+
+            output.flush();
+            output.close();
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Bundle resultData = new Bundle();
+        resultData.putInt("progress" ,100);
+        receiver.send(UPDATE_PROGRESS, resultData);
+    }
+}
